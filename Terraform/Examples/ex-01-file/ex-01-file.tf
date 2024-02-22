4ce9d10f2c555eb655cfec1f0558fcce83c13aa8
Franciel Rezende NT <francielrezende@gmail.com>
2024-02-22 19:04:07 -0300

    Adding more terraform exeamples

--- Terraform/Examples/ex-01-file/ex-01-file.tf
+++ Terraform/Examples/ex-01-file/ex-01-file.tf
@@ -5,11 +5,43 @@ resource "local_file" "pet" {
     file_permission = "0700"
 }
 
+resource "local_file" "pet_map" {
+    filename        = "/temp/pets_map.txt"
+    content         = var.file-content["content3"]
+}
 resource "local_sensitive_file" "pet-sensitive" {
     filename    = "/temp/pets-sensitive.txt"
     content     = "We love dogs!"
 }
 
-resource "auth0" "auth0-test" {
-  
+resource "random_pet" "my-pet" {
+    prefix      = "Mrs"
+    separator   = "."
+    length      = "1"
 }
+
+resource "local_file" "pet_var" {
+    filename    = var.filename
+    content     = var.content
+}
+
+resource "random_pet" "my-var-pet" {
+    prefix      = var.prefix
+    separator   = var.separator
+    length      = var.length
+}
+
+resource "random_pet" "my-list-pet" {
+    prefix      = var.prefix_list[1]
+    separator   = var.separator
+    length      = var.length
+}
+
+# resource "random_string" "random_pwd" {
+#     count       = "10"
+#     length      = "15"
+#     min_lower   = "2"
+#     min_numeric = "2"
+#     min_special = "1"
+#     min_upper   = "2"
+# }
