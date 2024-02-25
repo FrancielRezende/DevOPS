a551b708e8a852f71aa0de2def3a4d2e7d93c5a2
Franciel Rezende NT <francielrezende@gmail.com>
2024-02-25 19:52:09 -0300

    Creating multi-provider repo

---
+++ Terraform/Examples/multi-provider/main.tf
@@ -0,0 +1,9 @@
+resource "local_file" "pet_name" {
+	    content = "We love pets!"
+	    filename = "/root/pets.txt"
+}
+resource "random_pet" "my-pet" {
+	      prefix = "Mrs"
+	      separator = "."
+	      length = "1"
+}
