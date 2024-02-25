39db59195d2fe795f2f6b890f9bf44fbc240ee51
Franciel Rezende NT <francielrezende@gmail.com>
2024-02-25 19:50:09 -0300

    Creating a christmas-wishlist repo

---
+++ Terraform/Examples/christmas-wishlist/ps5.tf
@@ -0,0 +1,4 @@
+resource "local_file" "ps5" {
+  filename     = "/temp/ps5.txt"
+  content  = "And a PS5!"
+}
---
+++ Terraform/Examples/christmas-wishlist/xbox.tf
@@ -0,0 +1,4 @@
+resource "local_file" "xbox" {
+  filename     = "/temp/xbox.txt"
+  content  = "Wouldn't mind an XBox either!"
+}
