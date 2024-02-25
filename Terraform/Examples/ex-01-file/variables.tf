7d22bb2551a34ffd52b26817441796cb9b689b2c
Franciel Rezende NT <francielrezende@gmail.com>
2024-02-24 21:15:14 -0300

    Deleting workflow, including terraform commands

--- Terraform/Examples/ex-01-file/variables.tf
+++ Terraform/Examples/ex-01-file/variables.tf
@@ -1,41 +1,41 @@
 variable "filename" {
-    default     = "/temp/pets_var.txt"
-    type        = string
-    description = "Local where the file will be created"
+  default     = "/temp/pets_var.txt"
+  type        = string
+  description = "Local where the file will be created"
 }
 variable "content" {
-    default = "We love pets and vars!!"  
+  default = "We love pets and vars!!"
 }
-variable file-content {
-  type      = map
-  default   = {
+variable "file-content" {
+  type = map(any)
+  default = {
     "content1" = "We love pets"
     "content2" = "We love pets and vars!"
     "content3" = "We love pets and var maps!"
   }
 }
 variable "prefix" {
-    default = "Mr"  
+  default = "Mr"
 }
 variable "prefix_set" {
-    default     = ["Mr", "Mrs", "Sir"]
-    type        = set(string)
-    description = "Sets cannot have duplicated values"
+  default     = ["Mr", "Mrs", "Sir"]
+  type        = set(string)
+  description = "Sets cannot have duplicated values"
 
 }
 variable "prefix_list" {
-    default     = ["Mr", "Mrs", "Sir", "Sir"]
-    type        = list(string)
-    description = "Lists can have duplicated values"
+  default     = ["Mr", "Mrs", "Sir", "Sir"]
+  type        = list(string)
+  description = "Lists can have duplicated values"
 }
 variable "prefix_tuple" {
-    type        = tuple([string, number, bool])
-    default     = [ "value", 0, false ]
-    description = "Tuples can use multipe types of values"
+  type        = tuple([string, number, bool])
+  default     = ["value", 0, false]
+  description = "Tuples can use multipe types of values"
 }
 variable "separator" {
-    default = "." 
+  default = "."
 }
 variable "length" {
-    default = "2"  
+  default = "2"
 }
